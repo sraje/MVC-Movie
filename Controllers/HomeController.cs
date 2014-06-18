80ef3b1211ed651ad2fc44837932b2807aca087c
sraje <sraje@umail.ucsb.edu>
2014-06-18 11:46:26 -0700

    This is the entire project
    test

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is an attempt at deploying an asp.net site to ~the cloud~ oohhhhhhhh";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Please don't contact me";
+
+            return View();
+        }
+    }
+}
